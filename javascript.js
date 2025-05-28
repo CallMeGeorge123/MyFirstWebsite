0be4451371f157d6f2b10a7b1ea32ff2f4b47df7
CallMeGeorge123 <ge1760664@gmail.com>
2025-05-28 12:21:03 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,41 @@
+// Start
+console.log("Hello again, World!");
+
+let num1 = 4;
+var num2 = 21;
+const num3 = 123;
+
+
+// Arrays
+let arrayTest = [1, "Hello", 3.14, true]
+
+let PricelistTest = {
+    "Orange" : 5,
+    "Apple" : 3,
+    "Grapes" : 6,
+}
+
+console.log(PricelistTest["Grapes"])
+
+// Function
+function functionTest() {
+
+}
+
+functionTest()
+
+// Loops
+
+let running = 0;
+
+while (running < 10) {
+    console.log(running);
+    running++;
+}
+
+for (i = 1; i < 10; i++) {
+}
+
+function doSomething() {
+    document.getElementById("test").innerHTML = "Goodbye";
+}
